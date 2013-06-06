d5f271439239bfd4eda976d4b3e95da8e086c1af
Ben Schwarz <ben@germanforblack.com>
2013-06-06 18:31:48 +1000

    Updated grunt environment to use jshint, uglify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,8 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    min: {
-      dist: {
-        src: ['metaquery.js'],
-        dest: 'metaquery.min.js'
-      },
-
-      distjq: {
-        src: ['metaquery.jquery.js'],
-        dest: 'metaquery.jquery.min.js'
-      }
-    },
     qunit: {
       all: ['test/*.html']
-    },
-    lint: {
-      files: ['metaquery.js, metaquery.jquery.js']
     },
     watch: {
       files: '<config:lint.files>',
@@ -26,12 +12,22 @@ module.exports = function(grunt) {
     jshint: {
       all: ['Gruntfile.js', 'metaquery.js', 'metaquery.jquery.js']
     },
-    uglify: {}
+    uglify: {
+      options: {
+        report: 'gzip'
+      },
+      std: {
+        files: {
+          'metaquery.min.js' : 'metaquery.js',
+          'metaquery.jquery.min.js' : 'metaquery.jquery.js'
+        }
+      }
+    }
   });
 
   // Default task.
-  grunt.registerTask('default', ['qunit', 'jshint', 'min']);
+  grunt.registerTask('default', ['jshint', 'qunit', 'uglify']);
 
-  grunt.registerTask('travis', 'lint qunit');
+  grunt.registerTask('travis', ['jshint', 'qunit']);
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 };
