11f844d02908a9a471d27d6677d900ca4f860442
John Barton (joho) <jrbarton@gmail.com>
2013-06-13 13:19:53 +1000

    Added onBreakpointChange (single point to register events)

--- metaquery.js
+++ metaquery.js
@@ -1,16 +1,25 @@
 (function ( window, document ) {
   var metaQuery = {
     breakpoints: {},
-    _events: {},
+    _namedEvents: {},
     _eventMatchCache: {},
     _globalEvents: [],
-    bindGlobal: function ( fn ) {
-      metaQuery._globalEvents.push( fn );
+    onBreakpointChange: function () {
+      var args = Array.prototype.slice.call(arguments);
+      var fn = args.pop();
+      var name = args.pop();
+      console.log(name);
+      if ( typeof name === "undefined" ) {
+        metaQuery._globalEvents.push( fn );
+      } else {
+        ( metaQuery._namedEvents[name] = [] ).push( fn );
+      }
+      mqChange();
     },
     bind: function ( name, fn ) {
-      ( metaQuery._events[name] = [] ).push( fn );
-
-      mqChange();
+      // This is deprecated
+      //metaQuery.onBreakpointChange(name, fn);
+      ( metaQuery._namedEvents[name] = [] ).push( fn );
     }
   },
 
@@ -103,9 +112,9 @@
           matches = window.matchMedia( query ).matches;
 
       // Call events bound to a given breakpoint
-      if ( metaQuery._events[name] && metaQuery._eventMatchCache[name] !== matches ) {
-        for ( var i = 0; i < metaQuery._events[name].length; i++ ) {
-          var fn = metaQuery._events[name][i];
+      if ( metaQuery._namedEvents[name] && metaQuery._eventMatchCache[name] !== matches ) {
+        for ( var i = 0; i < metaQuery._namedEvents[name].length; i++ ) {
+          var fn = metaQuery._namedEvents[name][i];
           metaQuery._eventMatchCache[name] = matches;
 
           if ( typeof fn === 'function' ) { fn( matches ); }
@@ -166,4 +175,3 @@
   readyState( onDomReady );
 
 }( this, this.document ));
-
