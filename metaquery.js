e5753839da4e80208b5762dc858d5358306f8c3d
Ben Schwarz <ben@germanforblack.com>
2013-06-30 21:04:45 +1000

    * Use matchMedia listeners for internal api * Use metaQuery listeners for updateClasses/Images * Use nodeLists' live feature * Remove onResize handler

--- metaquery.js
+++ metaquery.js
@@ -1,38 +1,35 @@
 (function ( window, document ) {
   var metaQuery = {
     breakpoints: {},
-    _namedEvents: {},
-    _eventMatchCache: {},
     _globalEvents: [],
-    onBreakpointChange: function () {
-      var args = Array.prototype.slice.call( arguments ),
-          fn = args.pop(),
-          name = args.pop();
 
-      if ( typeof name === "undefined" ) {
-        metaQuery._globalEvents.push( fn );
-      } else {
-        ( metaQuery._namedEvents[name] = [] ).push( fn );
-      }
-      mqChange();
+    onBreakpointChange: function ( breakpointName, callback ) {
+      var fn = function ( mql ) {
+        var breakpoint = findBreakpointByQuery( mql.media ),
+            name = breakpoint['name'],
+            matches = breakpoint['mql'].matches;
+
+        callback( matches );
+        runGlobalCallbacks( matches, name );
+      };
+
+      metaQuery.breakpoints[breakpointName].addListener( fn );
+    },
+
+    onAnyBreakpointChange: function ( callback ) {
+      metaQuery._globalEvents.push( callback );
     }
   },
+
+  // Node lists are 'live', lets use that functionality
   metaTags = document.getElementsByTagName( 'meta' ),
+  elements = document.getElementsByTagName( 'img' ),
 
-  debounce = function( func, wait ) {
-    var args,
-        thisArg,
-        timeoutId;
-
-    function delayed() {
-      timeoutId = null;
-      func.apply( thisArg, args );
+  // Utilities
+  each = function ( collection, fn ) {
+    for ( var i = 0; i < collection.length; i++ ) {
+      fn( collection[i], i );
     }
-
-    return function() {
-      window.clearTimeout( timeoutId );
-      timeoutId = window.setTimeout( delayed, wait );
-    };
   },
 
   hasClass = function( element, className ) {
@@ -49,12 +46,18 @@
     }
   },
 
-  addClass = function(element, className) {
+  addClass = function( element, className ) {
     if ( !hasClass( element, className ) ) {
       element.className = ( element.className !== '' ) ? ( element.className + ' ' + className ) : className;
     }
   },
 
+  // End utilities
+
+  /*
+    Adds and removes breakpoint classes to the HTML element
+    depending on the media query match status
+  */
   updateClasses = function ( matches, name ) {
     var breakpoint = 'breakpoint-' + name,
         htmlNode = document.documentElement;
@@ -66,65 +69,67 @@
     }
   },
 
+  // Sets the img['src'] on images that have data-mq-src attributes
   updateElements = function ( matches, name ) {
     if ( !matches ) { return; }
 
-    var elements = document.getElementsByTagName( 'img' );
+    var setSrc = function ( element ) {
+      var template = element.getAttribute( 'data-mq-src' );
+      if ( template ) { element.src = template.replace( '[breakpoint]', name ); }
+    };
 
-    for ( var i = 0; i < elements.length; i++ ) {
-      var el = elements[i],
-          template = el.getAttribute( 'data-mq-src' );
+    each( metaQuery.breakpoints, setSrc );
+  },
 
-      if ( template ) {
-        el.src = template.replace( '[breakpoint]', name );
+  // Returns a metaQuery breakpoint
+  findBreakpointByQuery = function ( mq ) {
+    for ( var name in metaQuery.breakpoints ) {
+      var mql = metaQuery.breakpoints[name];
+      if ( mql.media === mq ) {
+        return {
+          name: name,
+          mql: mql
+        };
       }
     }
   },
 
-  // Called when a media query changes state
-  mqChange = function () {
-    for( var name in metaQuery.breakpoints ) {
-      var query = metaQuery.breakpoints[name],
-          matches = window.matchMedia( query ).matches;
-
-      // Call events bound to a given breakpoint
-      if ( metaQuery._namedEvents[name] && metaQuery._eventMatchCache[name] !== matches ) {
-        for ( var i = 0; i < metaQuery._namedEvents[name].length; i++ ) {
-          var fn = metaQuery._namedEvents[name][i];
-          metaQuery._eventMatchCache[name] = matches;
-
-          if ( typeof fn === 'function' ) { fn( matches ); }
-        }
-
-      }
-
-      // call any global events
-      for ( var j = 0; j < metaQuery._globalEvents.length; j++ ) {
-        var gfn = metaQuery._globalEvents[j];
-        if ( typeof gfn === 'function' ) { gfn( matches, name ); }
-      }
-
-      updateClasses( matches, name );
-      updateElements( matches, name );
-    }
+  runGlobalCallbacks = function ( match, breakpointName ) {
+    each( metaQuery._globalEvents, function ( callback ) {
+      callback( match, breakpointName );
+    });
   },
 
   collectMediaQueries = function () {
     // Add classes to the HTML node when a breakpoint matches
-    for ( var i = 0; i < metaTags.length; i++ ) {
-      if ( metaTags[i].name === 'breakpoint' ) {
-        var name = metaTags[i].getAttribute( 'content' ),
-            query = metaTags[i].getAttribute( 'media' );
+    each( metaTags, function ( tag ) {
+      if ( tag.name === 'breakpoint' ) {
+        var name = tag.getAttribute( 'content' ),
+            mq = tag.getAttribute( 'media' );
 
-        metaQuery.breakpoints[name] = query;
+        metaQuery.breakpoints[name] = window.matchMedia( mq );
+        metaQuery.onBreakpointChange( name, updateClasses );
+        metaQuery.onBreakpointChange( name, updateElements );
       }
+    });
+  },
+
+  // Called as a warm up check for breakpoints
+  globalCallbackInit = function () {
+    for( var name in metaQuery.breakpoints ) {
+      var matches = metaQuery.breakpoints[name].matches;
+      runGlobalCallbacks( matches, name );
     }
   },
 
   onDomReady = function () {
     collectMediaQueries();
-    mqChange();
+    globalCallbackInit();
   };
+
+  // Use onBreakpointChange API to power changes
+  metaQuery.onAnyBreakpointChange( updateClasses );
+  metaQuery.onAnyBreakpointChange( updateElements );
 
   // If the META tags are defined above this script,
   // we don't need to wait for domReady to set the breakpoint
@@ -133,8 +138,5 @@
 
   window['metaQuery'] = metaQuery;
   window.addEventListener( 'DOMContentLoaded', onDomReady );
-  window.addEventListener( 'resize', debounce( function () {
-    mqChange();
-  }, 50 ));
 
-}( this, this.document ));
+}( this, document ));
